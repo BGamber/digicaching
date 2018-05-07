521f31bcc92318811a88c3b8ba12aac968468c55
Terry Patterson <10890854+TerryRPatterson@users.noreply.github.com>
2018-05-07 12:58:09 -0400

    Fixed .env.example

--- Server/index.js
+++ Server/index.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use("/", (req, res, next) => {
+  let file = process.env.PUBLICDIR + req.url;
   res.sendFile(process.env.PUBLICDIR + req.url);
 });
 app.use((err, req, res, next) => {
@@ -19,4 +20,4 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(process.env.PORT, () => console.log("Server is now listening.", process.env.PORT));
+app.listen(process.env.PORT || 3000, () => console.log("Server is now listening.", process.env.PORT || 3000));
