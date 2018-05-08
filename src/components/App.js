10ef27a6f2fd145a84a5152733bd0193e90273a6
Terry Patterson <10890854+TerryRPatterson@users.noreply.github.com>
2018-05-08 14:48:26 -0400

    Started map component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import ProfileScreen from './ProfileScreen';
-import Footer from './Footer';
-import store from './store';
-import '../styles.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import ProfileScreen from "./ProfileScreen";
+import Footer from "./Footer";
+import store from "../store";
+import "../styles.css";
+import Map from "./Map";
 
 let App = () =>
   <Provider store={store}>
@@ -12,6 +13,8 @@ let App = () =>
       <div className="App">
         <Route exact path="/profile" component={ProfileScreen} />
         <Footer />
+        <Map caches={[{longitude:-84.37277,latitude:33.847279, name:"Farm Burger"},
+          {latitude:33.848460,longitude:-84.37360, name:"ATV"}]}/>
       </div>
     </Router>
   </Provider>;
