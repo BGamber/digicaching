26a4892a9c58e21250fca6be91620c0d4296c7d9
Ben Gamber <ben.gamber@gmail.com>
2018-05-08 14:51:21 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import ProfileScreen from './ProfileScreen';
 import Footer from './Footer';
-import from './index.css';
 import store from '../store';
+import from './index.css';
 
 let App = () =>
   <Provider store={store}>
