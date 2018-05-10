4593e53060dfc8ce606fd766773fcd9d178df951
Ben Gamber <ben@stickfishstudio.com>
2018-05-10 15:37:08 -0400

    Fix user-by-id and item-by-id requests

--- server/database.js
+++ server/database.js
@@ -15,15 +15,15 @@ let getUserByName = async name => {
 };
 
 let getUserById = async (req, res) => {
-  let queryString = "SELECT email, name, image_url, password FROM users WHERE id = $1;";
-  let user = await db.one(queryString, req.params.id);
-  res.send(user);
+  let queryString = "SELECT email, name, image_url, password FROM users" + (req.params.id !== undefined ? " WHERE id = $1" : "") +";";
+  let users = await db.query(queryString, [req.params.id]);
+  res.send(users);
 };
 
-let getItems = (req, res) => {
-  let queryString = "SELECT name, description, image_url FROM items" + () +";";
-  let user = await db.query(queryString, [name]);
-  return user;
+let getItems = async (req, res) => {
+  let queryString = "SELECT name, description, image_url FROM items" + (req.params.id !== undefined ? " WHERE id = $1" : "") +";";
+  let items = await db.query(queryString, [req.params.id]);
+  res.send(items);
 };
 
 let getCollections = (req, res) => {
