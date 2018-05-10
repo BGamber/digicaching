1a52150c638aae383510408a2f092bfee7496002
Itzik Shaoulian <itzikshaoulian@gmail.com>
2018-05-10 17:45:17 -0400

    bug fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ import '../index.css';
 
 let Footer = () => {
   return (
-    <div className="navber-div">
+    <div className="navbar-div">
       <nav className="navbar">
-        <div><NavLink to="/main/profile"><i className="ion-android-contact"></i></NavLink></div>
+        <div><NavLink to="/"><i className="ion-android-contact"></i></NavLink></div>
         <div><NavLink to="/main/map"><i className="ion-map"></i></NavLink></div>
         <div><NavLink to="/main/collections"><i className="ion-aperture"></i></NavLink></div>
         <div><NavLink to="/main/friends-lookup"><i className="ion-person-add"></i></NavLink></div>
--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,35 +1,36 @@
-import React from "react";
-import {Route, Switch} from "react-router-dom";
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 
-import Footer from "./Footer";
-import ProfileScreen from "./ProfileScreen";
-import MyMapComponent from "./Map";
-import Collections from "./CollectionScreen";
-import FriendsLookup from "./friends-lookup";
-import SearchPage from "./SearchPage";
+import Footer from './Footer';
+// import ProfileScreen from './ProfileScreen';
+import ProfileScreen from './profilePage';
+import MyMapComponent from './Map';
+import Collections from './CollectionScreen';
+import FriendsLookup from './friends-lookup';
+import SearchPage from './SearchPage';
 
 let MainScreen = () => {   
-  return(
-    <div className='route-container'>
-      <div className="content-container">
-        <Switch>
-          <Route exact path='/main/profile' component={ProfileScreen}/>
-          <Route exact path='/main/map' component={MyMapComponent} />
-          <Route exact path='/main/collections' component={Collections} />
-          <Route exact path='/main/friends-lookup' component={FriendsLookup} />
-          <Route exact path='/main/search' component={SearchPage} />
+    return(
+        <div className='route-container'>
+            <div className="content-container">
+            <Switch>
+                {/* <Route exact path='/main/profile' component={ProfileScreen}/> */}
+                <Route exact path='/main/map' component={MyMapComponent} />
+                <Route exact path='/main/collections' component={Collections} />
+                <Route exact path='/main/friends-lookup' component={FriendsLookup} />
+                <Route exact path='/main/search' component={SearchPage} />
                 
                 
-          {/* <Route exact path='/main/something/:somthing render={ (props) => {
+                {/* <Route exact path='/main/something/:somthing render={ (props) => {
                     let profileUserId = props.match.params.userid
                     return <ProfilePage profileUserId={profileUserId}/>
                 }} /> */}
 
-        </Switch>
-      </div>
-      <Footer />
-    </div>
-  );
-};
+            </Switch>
+            </div>
+            <Footer />
+        </div>
+    )
+}
 
 export default MainScreen;
