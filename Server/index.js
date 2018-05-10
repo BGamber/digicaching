fe44fd499c1df7e98ddfa8132552711ef1ecc0e3
Ben Gamber <ben@stickfishstudio.com>
2018-05-10 14:02:41 -0400

    Add JWT check to api requests

--- Server/index.js
+++ Server/index.js
@@ -45,7 +45,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded());
-app.use("/api", api);
+app.use("/api", checkToken, api);
 app.use("/auth", auth);
 app.use("/", (req, res, next) => {
   let file = process.env.PUBLICDIR + req.url;
