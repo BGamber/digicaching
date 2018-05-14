89ec88e306306e8538e96fc683106c16bfba5ffa
Kyle Booth <kbooth1000@gmail.com>
2018-05-14 16:53:16 -0400

    Fetch sends proper headers

--- src/components/profilePage.js
+++ src/components/profilePage.js
@@ -10,38 +10,29 @@ import '../index.css';
 class ProfilePage extends Component {
   async componentDidMount() {
     console.log('this.props.users: ', this.props.users);
-    console.log('this.props.users.id: ', this.props.users[0].id);
-    // let itemsList = await getUserInventories();
+    console.log('this.props.users[0].id: ', this.props.users[0].id);
+    let authToken = this.props.auth;
+    console.log('authToken: ', authToken);
 
-    let tempUserInfo = {
-      name: 'MrUserT',
-      friendsCount: 100, //indeed they do
-      ranking: 18,
-      score: 42444
-    }
-    // let uid = "096780a6-3347-410c-98d4-48db176ce9b1";
-
-    fetch(`${process.env.REACT_APP_BACKEND}/api/users/${this.props.users[0].id}`)
+    fetch(`${process.env.REACT_APP_BACKEND}/api/users/${this.props.users[0].id}`, {
+      "headers": {
+        "authorization": "Bearer "+authToken
+      }
+    })
       .then((res) => {
         res.json()
           .then((data) => {
             console.log('data: ', data);
             console.log('this.props: ', this.props);
-            this.props.setCurrentProfile(data ? data : tempUserInfo);
+            this.props.setCurrentProfile(data);
 
           })
       })
-    // this.props.setCurrentProfile(uid);
   }
 
   render() {
-    // let uid = "096780a6-3347-410c-98d4-48db176ce9b1";
     let currentUser = this.props.users;
-    //   this.props.users.find(user => user.id === uid);
-    // console.log('this.uid: ', uid);
-
     console.log('currentUser:::: ', currentUser);
-    
     return (
       <div className="profile-page" >
 
@@ -58,7 +49,7 @@ class ProfilePage extends Component {
 
 let mapStateToProps = state => ({
   users: state.users,
-  auth: state.auth
+  auth: state.activeUserToken
 });
 
 let mapDispatchToProps = (dispatch) => {
