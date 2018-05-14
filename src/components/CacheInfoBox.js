0afc9b79367282b2e3e82141412c90a41f5e8ecf
Terry Patterson <10890854+TerryRPatterson@users.noreply.github.com>
2018-05-14 12:02:58 -0400

    Working on infobox

--- src/components/CacheInfoBox.js
+++ src/components/CacheInfoBox.js
@@ -2,16 +2,33 @@ import React from "react";
 import moment from "moment";
 import PropTypes from "prop-types";
 import ClaimButton from "./ClaimButton";
+import {OverlayView} from "react-google-maps";
+
+
+let getPixelPositionOffset = (width, height) => ({
+  x: 16,
+  y: -(height / 2),
+});
 
 
 let CacheInfoBox = ({createdOn, claimedOn, name, lat, lng}) => {
 
   return (
-    <div>
-      <h3>{name}</h3>
-      <p className="timestamp creation">Cached created:<time dateTime={createdOn}>{moment(createdOn).fromNow(0)}</time></p>
-      <p className="timestamp claim">Cached claimed:<time dateTime={claimedOn}>{moment(claimedOn).fromNow(0)}</time></p>
-      <ClaimButton lat={lat} lng={lng}/>
+    <OverlayView
+        position={marker.position}
+        mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+        getPixelPositionOffset={getPixelPositionOffset}
+      >
+        <div className="poi">
+          <h3>{name}</h3>
+          <p className="timestamp creation">Cached created:
+            <time dateTime={createdOn}>{moment(createdOn).fromNow(0)}</time></p>
+          <p className="timestamp claim">Cached claimed:
+            <time dateTime={claimedOn}>{moment(claimedOn).fromNow(0)}</time></p>
+          <ClaimButton lat={lat} lng={lng}/>
+        </div>
+      </OverlayView>
+
     </div>);
 };
 
