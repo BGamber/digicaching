54e353eadaf22f42c8064d1821fd37df067ff6db
Terry Patterson <10890854+TerryRPatterson@users.noreply.github.com>
2018-05-15 12:48:00 -0400

    Added activeUserId to redux store, and fixed enter key on login screen

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,9 +1,17 @@
 export const setTokenConst = "ACTIVE_USER_TOKEN";
+export const setIDConst = "ACTIVE_USER_ID";
 
 let setActiveUserToken = (token) => {
-  return {type:setTokenConst, token:token};
+  return {type:setTokenConst, token};
 };
 
 setActiveUserToken.toString = () => setTokenConst;
+
+export let setActiveUserID = (id) => {
+  return {type:setIDConst, id};
+};
+
+setActiveUserID.toString = () => setIDConst;
+
 
 export default setActiveUserToken;
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,13 +1,19 @@
-import setUserToken, {setTokenConst} from "../actions/userActions";
+import {setTokenConst, setIDConst} from "../actions/userActions";
 
 let setUserTokenReducer = (state,action) => {
   let token = action["token"];
   return {...state, activeUserToken:token};
 };
 
+let setUserIDReducer = (state, action) => {
+  let {id} = action;
+  return {...state, activeUserID:id};
+};
+
 
 let reducers = {
-  [setUserToken]:setUserTokenReducer
+  [setTokenConst]:setUserTokenReducer,
+  [setIDConst]: setUserIDReducer
 };
 
 let userReducer = (state, action) => {
@@ -15,8 +21,8 @@ let userReducer = (state, action) => {
   return reducers[type](state,action);
 };
 
-
-setUserTokenReducer.toString = () => setUserToken;
-userReducer.toString = () => setTokenConst;
+setUserIDReducer.toString = () => setIDConst;
+setUserTokenReducer.toString = () => setTokenConst;
+userReducer.toString = () => "ACTIVE_USER_";
 
 export default userReducer;
