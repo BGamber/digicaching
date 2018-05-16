da0035bb9cee2f4117f75eff912e7e40b3aa3287
Itzik Shaoulian <itzikshaoulian@gmail.com>
2018-05-16 10:54:09 -0400

    friends lookup styling

--- src/components/FriendsPage.js
+++ src/components/FriendsPage.js
@@ -11,18 +11,17 @@ class FriendsPage extends Component {
           <h1 className="Players">Active Players</h1>
           <div className="friend-card">
             <div className="friend-properties">
-              <h1 className="friend-name">User Name: {this.props.searchedUser.name}</h1>
-              <h1 className="friend-email">User Email: {this.props.searchedUser.email}</h1>
-              {
-                (this.props.searchedUser.image_url)?
-                <img className="friend-image" alt="user-image" src={this.props.searchedUser.image_url} />:
-                <img className="friend-image-notFound" alt="user-image" src={this.props.searchedUser.image_url} />
-              } 
-              
-            </div>
-            <div className="add-friend">
+              <h1 className="user-name">User Name:</h1> 
+              <h3 className="friend-name">{this.props.searchedUser.name}</h3>
+              <h1 className="user-email">User Email:</h1>
+              <h3 className="friend-email">{this.props.searchedUser.email}</h3>
               <button className="add-friend-button">Add-Friend</button>
             </div>
+            {
+              (this.props.searchedUser.image_url)?
+              <img className="friend-image" alt="user-image" src={this.props.searchedUser.image_url} />:
+              <img className="friend-image-notFound" alt="user-image" src={this.props.searchedUser.image_url} />
+            } 
           </div>
           <Link to="/friends-lookup"><button className="back-button2">Back</button></Link>
       </div>
