bf856f9ba8333e5845adb2b389678bea099dab84
Itzik Shaoulian <itzikshaoulian@gmail.com>
2018-05-16 11:34:13 -0400

    Fixing lookup page bugs

--- src/components/FriendsPage.js
+++ src/components/FriendsPage.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import FriendCall from './FriendCall';
 import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -9,20 +10,9 @@ class FriendsPage extends Component {
     return (
       <div className="listOfFriends">
           <h1 className="Players">Active Players</h1>
-          <div className="friend-card">
-            <div className="friend-properties">
-              <h1 className="user-name">User Name:</h1> 
-              <h3 className="friend-name">{this.props.searchedUser.name}</h3>
-              <h1 className="user-email">User Email:</h1>
-              <h3 className="friend-email">{this.props.searchedUser.email}</h3>
-              <button className="add-friend-button">Add-Friend</button>
-            </div>
-            {
-              (this.props.searchedUser.image_url)?
-              <img className="friend-image" alt="user-image" src={this.props.searchedUser.image_url} />:
-              <img className="friend-image-notFound" alt="user-image" src={this.props.searchedUser.image_url} />
-            } 
-          </div>
+          {this.props.searchedUsers.map((friend)=> {
+            return <FriendCall friend={friend} />
+          })}
           <Link to="/friends-lookup"><button className="back-button2">Back</button></Link>
       </div>
     );
@@ -30,7 +20,7 @@ class FriendsPage extends Component {
 };
 
 let mapStateToProps = (state) => ({
-  searchedUser: state.searchedFriend
+  searchedUsers: state.searchedFriend
 })
 
 export default connect(mapStateToProps)(FriendsPage);
