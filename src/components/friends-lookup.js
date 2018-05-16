856c4669749209af7a96ed6d7da05d23590221de
Itzik Shaoulian <itzikshaoulian@gmail.com>
2018-05-16 10:17:49 -0400

    make the friend lookup works

--- src/components/friends-lookup.js
+++ src/components/friends-lookup.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setCurrentFriends } from '../actions/friendActions';
+import { Link, withRouter } from 'react-router-dom';
 
 class FriendsLookup extends Component {
   constructor(props){
@@ -8,9 +11,31 @@ class FriendsLookup extends Component {
     }
   }
 
+  submitHandler = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:5000/api/users', {
+      headers: {
+        authorization: `Bearer ${this.props.jwt}`
+      }
+    })
+
+    .then(data => { 
+      return data.json()})
+    .then(users => {
+      let theUser = users.find(user => user.name.toUpperCase() === this.state.friendName.toUpperCase())
+      if(!theUser){
+        return alert("User not found")
+      } else {
+        console.log(theUser)
+        this.props.setSearchedFriend(theUser)
+        this.props.history.push('/friends');
+      }
+    })
+  }
+
   render(){
     return (
-      <form className="friends">
+      <form  onSubmit={this.submitHandler} className="friends">
           <h1 className="lookup-title">Find A Friend: </h1>
           <input 
             type="text" 
@@ -25,4 +50,16 @@ class FriendsLookup extends Component {
   }
 };
 
-export default FriendsLookup;
+
+let mapStateToProps = (state) => ({
+  jwt: state.activeUserToken
+})
+
+let mapDispatchToProps = dispatch => ({
+  setSearchedFriend: (friend) => {
+    dispatch(setCurrentFriends(friend))
+  } 
+})
+
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(FriendsLookup));
