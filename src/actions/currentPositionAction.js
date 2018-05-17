b08066cb8c1287e09084bfc92ef54a13f7374911
Terry Patterson <10890854+TerryRPatterson@users.noreply.github.com>
2018-05-17 13:34:17 -0400

    Might Fix it

--- src/actions/currentPositionAction.js
+++ src/actions/currentPositionAction.js
@@ -1,4 +1,5 @@
 import authFetch from "../lib/authFetch";
+import {previousBounds} from "../actions/uiActions";
 
 export let setPosition = (newLat, newLng) => {
   return {type:"POSITION_SET",currentLat:newLat, currentLng:newLng };
@@ -9,15 +10,20 @@ setPosition.toString = () => "POSITION_SET";
 
 export let newBounds = ({east, west, north, south}) => {
   return (dispatch, getState) => {
-    let {currentLat, currentLng} = getState();
-    authFetch(`${process.env.REACT_APP_BACKEND}/api/caches?loc=${currentLat},`+
+    let {currentLat, currentLng, bounds} = getState();
+    let {northP, southP, eastP, westP} = bounds;
+    if (northP !== north && southP !== south && eastP !== east
+      && westP !== west) {
+      dispatch(previousBounds({north,south,east,west}));
+      authFetch(`${process.env.REACT_APP_BACKEND}/api/caches?loc=${currentLat},`+
       `${currentLng}&bounds=${north},${south},${west},${east}`).then(res => {
-      if (res.status === 200) {
-        res.json().then((caches) => {
-          dispatch(setCaches(caches));
-        });
-      }
-    });
+        if (res.status === 200) {
+          res.json().then((caches) => {
+            dispatch(setCaches(caches));
+          });
+        }
+      });
+    }
   };
 };
 
